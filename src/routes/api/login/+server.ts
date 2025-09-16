cf1e30c650caacd9654e770e1cf90c67a2d2eea5
Siri Yeung <491763280@qq.com>
2025-09-16 17:57:26 +0000

    login to save username to supabase

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -3,21 +3,31 @@ import type { RequestHandler } from './$types';
 import { sendRequest } from '$lib/utils/api';
 import { generateToken } from '$lib/utils/jwt';
 import { API_HOST } from '$env/static/private';
+import { supabase } from '$lib/supabase';
 
 export const POST: RequestHandler = async ({ request }) => {
-  const { username, password } = await request.json();
+  try {
+    const { username, password } = await request.json();
 
-  const url = `${API_HOST}/goip_get_sms_stat.html?username=${username}&password=${password}`;
+    const url = `${API_HOST}/goip_get_sms_stat.html?username=${username}&password=${password}`;
 
-  const response = await sendRequest(url);
-  console.log(response.data.stats);
-  const { data: {code, reason}, success } = response;
-  
-  if (code === 1) {
-    return json({ success: false, message: reason }, { status: 401 });
+    const response = await sendRequest(url);
+    console.log(response.data.stats);
+    const { data: {code, reason}, success } = response;
+    
+    if (code === 1) {
+      return json({ success: false, message: reason }, { status: 401 });
+    }
+
+    const jwt = generateToken(username, password);
+
+    const {data,error} = await supabase.from('user_profiles').upsert([
+            { username, last_login: new Date() }
+          ], { onConflict: 'username' });
+
+    return json({ success: true, message: 'Login successful', jwt }, { status: 200 }); 
+  } catch (error) {
+    console.error(error);
+    return json({success: false,  message: 'Opppss something went wrong'}, { status: 500  });
   }
-
-  const jwt = generateToken(username, password);
-
-  return json({ success: true, message: 'Login successful', jwt }, { status: 200 });
 };
