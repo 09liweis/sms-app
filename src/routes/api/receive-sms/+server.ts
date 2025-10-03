b6b2203aeb981e909409468e3a1b9710b0447984
Siri Yeung <491763280@qq.com>
2025-10-03 14:48:55 +0000

    update receive sms api filter

--- src/routes/api/receive-sms/+server.ts
+++ src/routes/api/receive-sms/+server.ts
@@ -21,7 +21,7 @@ export const POST: RequestHandler = async ({ request, url }) => {
     };
     console.log('Parsed SMS:', parsedBody);
 
-    const {error:updateError} = await supabase.from('messages').update({is_new:false}).eq('ip',ip).eq('sender',parsedBody.sender).eq('port',parsedBody.port);
+    const {error:updateError} = await supabase.from('messages').update({is_new:false}).eq('ip',ip).eq('is_new',true).eq('sender',parsedBody.sender).eq('port',parsedBody.port);
     const {error} = await supabase.from('messages').insert(parsedBody);
     if (error) {
       console.error('Insert receive sms error: ', error);
