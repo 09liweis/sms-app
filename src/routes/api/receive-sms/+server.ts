87aa22df696693c849562fd617300ac61d6facb4
Siri Yeung <491763280@qq.com>
2025-10-03 02:38:50 +0000

    receive sms api to store msg

--- src/routes/api/receive-sms/+server.ts
+++ src/routes/api/receive-sms/+server.ts
@@ -1,8 +1,10 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { supabase } from '$lib/supabase';
 
-export const POST: RequestHandler = async ({ request }) => {
+export const POST: RequestHandler = async ({ request, url }) => {
   try {
+    const ip = url.searchParams.get('url');
     const body = await request.text();
     const lines = body.split('\n');
     const lastLine = lines[lines.length - 1];
@@ -11,10 +13,18 @@ export const POST: RequestHandler = async ({ request }) => {
       "receiver": body.match(/Receiver: "([^"]+)"/)?.[1],
       "SMSC": body.match(/SMSC: (\d+)/)?.[1],
       "SCTS": body.match(/SCTS: (\d+)/)?.[1],
-      "Slot": body.match(/Slot: "([^"]+)"/)?.[1],
-      "Message": lastLine
+      "port": body.match(/Slot: "([^"]+)"/)?.[1],
+      "message": lastLine,
+      ip,
+      type: 'received'
     };
     console.log('Parsed SMS:', parsedBody);
+    
+    const {error} = await supabase.from('messages').insert(parsedBody);
+    if (error) {
+      console.error('Insert receive sms error: ', error);
+      return json({ success: false, message: error.message }, { status: 500  }); 
+    }
     return json({ success: true, message: 'SMS received successfully', data: parsedBody });
   } catch (error) {
     console.error('Error receiving SMS:', error);
